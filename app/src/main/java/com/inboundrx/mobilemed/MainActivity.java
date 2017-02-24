a3e2a22b83a93c6e1944e87e3cef6b96fbe5c7a4
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-02-23 16:00:16 -0800

    connect navigation elements with activities for walkthru

--- app/src/main/java/com/inboundrx/mobilemed/MainActivity.java
+++ app/src/main/java/com/inboundrx/mobilemed/MainActivity.java
@@ -1,13 +1,29 @@
 package com.inboundrx.mobilemed;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+  private Button mLogin;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mLogin = (Button) findViewById(R.id.buttonSignin);
+
+    mLogin.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if(v == mLogin) {
+          Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+          startActivity(intent);
+        }
+      }
+    });
   }
 }
--- app/src/main/java/com/inboundrx/mobilemed/MenuActivity.java
+++ app/src/main/java/com/inboundrx/mobilemed/MenuActivity.java
@@ -1,7 +1,11 @@
 package com.inboundrx.mobilemed;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MenuActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_menu);
+
+    ButterKnife.bind(this);
+  }
+
+  @OnClick(R.id.buttonPatient)
+  public void viewPatient() {
+    Intent intent = new Intent(MenuActivity.this, PatientActivity.class);
+    startActivity(intent);
+  }
+
+  @OnClick(R.id.buttonVitals)
+  public void viewVitals() {
+    Intent intent = new Intent(MenuActivity.this, VitalsActivity.class);
+    startActivity(intent);
+  }
+
+  @OnClick(R.id.buttonVideo)
+  public void viewVideo() {
+    Intent intent = new Intent(MenuActivity.this, VideoActivity.class);
+    startActivity(intent);
   }
 }
