a5d57c016ab23ddfce3dc5e0d2dae95dbbed2a2c
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-02-23 21:26:01 -0800

    add phone intent

--- app/src/main/java/com/inboundrx/mobilemed/MainActivity.java
+++ app/src/main/java/com/inboundrx/mobilemed/MainActivity.java
@@ -1,13 +1,16 @@
 package com.inboundrx.mobilemed;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
   private Button mLogin;
+  private TextView mPhoneLabel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +18,21 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     mLogin = (Button) findViewById(R.id.buttonSignin);
+    mPhoneLabel = (TextView) findViewById(R.id.textViewPhone);
 
     mLogin.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        if(v == mLogin) {
-          Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-          startActivity(intent);
-        }
+        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+        startActivity(intent);
+      }
+    });
+
+    mPhoneLabel.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "18666245633"));
+        startActivity(phoneIntent);
       }
     });
   }
